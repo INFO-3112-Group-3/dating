c206b341beec85e25dffac8c871d3958b617cacf
Anton Yashchenko <ntondev@gmail.com>
2026-03-05 02:17:37 -0500

    Added DatabaseSetting class to loead .appconfig data.

---
+++ backend/FindIT.Api/Models/DatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace FindIT.Api.Models
+{
+    // sed to store the appsettings.json file's BookStoreDatabase property values.
+    // The JSON and C# property names are named identically to ease the mapping process.
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string UsersCollectionName { get; set; } = null!;
+    }
+}
