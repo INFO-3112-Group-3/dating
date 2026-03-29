03b8dac0948694203973fc9832fa0b7748dbda58
Amon Ramsden <a_ramsden203976@fanshaweonline.ca>
2026-03-29 13:29:18 -0400

    Revert "Updated Update DTO to allow for updates"
    This reverts commit dd634fe0fecf445df11c42848ba1f3fe5d280167.

--- backend/FindIT.Api/DTOs/UpdateUserDto.cs
+++ backend/FindIT.Api/DTOs/UpdateUserDto.cs
@@ -5,25 +5,12 @@ namespace FindIT.Api.DTOs;
 // This DTO is used for updating user profiles. All fields are optional to allow partial updates.
 public class UpdateUserDto
 {
-    public string? Id {get;set;}
-    public string Email { get; set; } = null!;
-    public bool IsPaidUser { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    public string? Salutation {get; set;}
-    public string? ContactInfo {get; set;}
-    public string? ContactMethod {get; set;}
     public Gender? Gender { get; set; }
-
-    public int Age { get; set; }
-
+    public int? Age { get; set; }
     public string? City { get; set; }
     public string? Region { get; set; }
-
-    // GeoJSON Point: [Longitude, Latitude]
-
-    public List<string> Skills { get; set; } = new();
-
 
     public UserPreferences? Preferences { get; set; }
 }
