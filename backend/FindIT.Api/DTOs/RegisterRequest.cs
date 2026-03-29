af9d1f81f976a423aadda22c08339e9556300929
Broden-Pifher <brodenpifher@gmail.com>
2026-03-29 13:33:09 -0400

    RegisterRequest

--- backend/FindIT.Api/DTOs/RegisterRequest.cs
+++ backend/FindIT.Api/DTOs/RegisterRequest.cs
@@ -1,15 +1,10 @@
-﻿using FindIT.Api.Models;
-using System.Text.Json.Serialization;
-
-namespace FindIT.Api.DTOs;
+﻿namespace FindIT.Api.DTOs;
 
 // This DTO is used for user registration. It includes necessary fields for creating a new user account.
 public class RegisterRequest
 {
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
-    [JsonConverter(typeof(DateOnlyJsonConverter))]
-    public DateTime DateOfBirth { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Salutation {get; set;}
